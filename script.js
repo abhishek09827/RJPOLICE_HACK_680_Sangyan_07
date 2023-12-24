86bde6a9bb07711cf9a36aa1f7e39f5765cc48dc
suryansh-17 <suripandey17@gmail.com>
2023-12-25 00:55:33 +0530

    page added and login button active

---
+++ script.js
@@ -0,0 +1,8 @@
+"use strict";
+//elements
+let btn = document.querySelector(".submit-btn");
+
+btn.onclick = function () {
+  console.log(`hii`);
+  window.open("inputPage/mainPage.html", "_top");
+};
