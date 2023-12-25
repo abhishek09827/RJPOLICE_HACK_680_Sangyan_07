7267526325afc63c75408c830e8c0983beb1ef78
suryansh-17 <suripandey17@gmail.com>
2023-12-25 13:56:45 +0530

    password set to 111 to go to next page

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 "use strict";
 //elements
 let btn = document.querySelector(".submit-btn");
+let usernameInput = document.querySelector(".login--username");
+let passwordInput = document.getElementById("exampleInputPassword1");
+let wrongPassDiv = document.getElementById("wrong-pass-text");
 
-btn.onclick = function () {
-  console.log(`hii`);
-  window.open("inputPage/mainPage.html", "_top");
-};
+btn.addEventListener("click", function () {
+  if (usernameInput.value === "user" && passwordInput.value === "1111") {
+    window.open("inputPage/mainPage.html", "_top");
+  } else {
+    wrongPassDiv.classList.remove("opacity-0");
+  }
+});
